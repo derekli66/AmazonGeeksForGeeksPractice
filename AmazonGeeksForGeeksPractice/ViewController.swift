4134d2c867d260db3f07ca06381b95bc4065614c
Derek Lee <derek.lee@positivegrid.com>
2022-08-19 19:01:38 +0800

    Initial Commit

---
+++ AmazonGeeksForGeeksPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmazonGeeksForGeeksPractice
+//
+//  Created by CHIEN-MING LEE on 2022/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
